6a0aafe2723308dd0fdf5467c61470103f271916
Akim de Jesus Galicia <akimdjg@gmail.com>
2023-01-02 11:48:32 -0600

    init web service demo

---
+++ restful-web-services/src/test/java/com/akim/rest/ws/restfulwebservices/RestfulWebServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.akim.rest.ws.restfulwebservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulWebServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
